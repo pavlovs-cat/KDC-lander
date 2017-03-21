0bd3a268a1d2122f386935c0eac47d5ad3ed15ca
pavlovs-cat <catherinepavlov@gmail.com>
2017-03-21 16:53:52 -0400

    Attempts to rotate lander about z axis

--- lander/dynamics.c
+++ lander/dynamics.c
@@ -340,12 +340,12 @@ void reinit_dynamics( SIM *s )
 
   // initialize alien artifact velocity
   // change w to see how tumbling depends on I
-  sdfast_state[ALIEN_XD] = 0.05;
-  sdfast_state[ALIEN_YD] = 0.02;
-  sdfast_state[ALIEN_ZD] = 0.01;
-  sdfast_state[ALIEN_WX] = 0.3/3;
-  sdfast_state[ALIEN_WY] = 1.0/3;
-  sdfast_state[ALIEN_WZ] = 0.1/3;
+  sdfast_state[ALIEN_XD] = 0.0;
+  sdfast_state[ALIEN_YD] = 0.0;
+  sdfast_state[ALIEN_ZD] = 0.0;
+  sdfast_state[ALIEN_WX] = -0.01;
+  sdfast_state[ALIEN_WY] = 0.1;
+  sdfast_state[ALIEN_WZ] = 01.0;
 
   set_alien_sdfast_state( s, sdfast_state );
 
