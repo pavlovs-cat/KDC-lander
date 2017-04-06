11eefd85522fc0a7af694e14fb45512154c634cb
Lauren Lieu <laurenlieu@Laurens-MacBook.wv.cc.cmu.edu>
2017-04-06 14:41:57 -0400

    Part 2D: moment of inertia calculation A*p=0 tested and working. Modify 'frames' in pt2A_rot.m to use different frames in sequence

--- matlab_code/pt2A_rot.m
+++ matlab_code/pt2A_rot.m
@@ -4,7 +4,6 @@
 % File name to read data from
 data_file = 'd00063-default';
 
-
 % 100Hz for 10 seconds
 samplingRate = 100;
 tau = 1/samplingRate;
@@ -19,7 +18,7 @@ N = length(times)-1;
 pos_vec = extract_data(data_file, 'ml', n, 1:(N+1));
 
 % Generate COMs
-%[com, vel] = optim_com();
+[com, vel] = optim_com();
 
 % store quaternions for each timestamp
 artifact_rot = zeros(N,4);
@@ -31,6 +30,7 @@ th2(:, 1) = [1 0 0 0]';
 Rw2l = quat2rotm([0 0 0 1]);
 Rots(:, :, 1) = eye(3); 
 com0 = com;
+
 for j = 2:N+1
     com1 = com0 + vel*tau;
     p = reshape(pos_vec(j-1, :),3,8);
@@ -41,21 +41,22 @@ for j = 2:N+1
     th(:, j-1) = rotm2quat(Rots(:,:,j));
     com0 = com1;
 end
+
 th2 = th2(:, 2:end);
 th2 = [th2(1, :); -th2(3, :); th2(2, :); th2(4, :)];
 Rots = Rots(:, :, 2:end);
 th_sim =  extract_data(data_file, 'a_q', {'0', '1', '2', '3'}, 1:N)';
+
 % Plot velocities for comparison
-plot_results(th_sim(1, :), th2(1, :), times(1:N), 'q_0');
-plot_results(th_sim(2, :), th2(2, :), times(1:N), 'q_1');
-plot_results(th_sim(3, :), th2(3, :), times(1:N), 'q_2');
-plot_results(th_sim(4, :), th2(4, :), times(1:N), 'q_3');
+% plot_results(th_sim(1, :), th2(1, :), times(1:N), 'q_0');
+% plot_results(th_sim(2, :), th2(2, :), times(1:N), 'q_1');
+% plot_results(th_sim(3, :), th2(3, :), times(1:N), 'q_2');
+% plot_results(th_sim(4, :), th2(4, :), times(1:N), 'q_3');
 
 % Extract simulated ang accels and velocities
 w_sim = extract_data(data_file, 'a_w', {'x', 'y', 'z'}, 1:N)';
 
 % Convert angv to assignment frame
-
 for idx = 1 : N
     Rots(:, :, idx) = quat2rotm(th2(:, idx)');
 end
@@ -65,9 +66,9 @@ w_vec = pt2B_angv(Rots, tau);
 w_vec2 = pt2B_angv2(Rots, tau);
 
 % Plot velocities for comparison
-plot_results(w_sim(1, :), w_vec2(1, :), times(1:N), 'w_x');
-plot_results(w_sim(2, :), w_vec2(2, :), times(1:N), 'w_y');
-plot_results(w_sim(3, :), w_vec2(3, :), times(1:N), 'w_z');
+% plot_results(w_sim(1, :), w_vec2(1, :), times(1:N), 'w_x');
+% plot_results(w_sim(2, :), w_vec2(2, :), times(1:N), 'w_y');
+% plot_results(w_sim(3, :), w_vec2(3, :), times(1:N), 'w_z');
 
 % Compute angular accelerations
 w_dot_vec = pt2C_angacc(w_vec2, tau);
@@ -77,9 +78,9 @@ w_dot_vec = pt2C_angacc(w_vec2, tau);
 %plot_results(w_dot_sim(2, :), w_dot_vec(2, :), times, 'w_y');
 %plot_results(w_dot_sim(3, :), w_dot_vec(3, :), times, 'w_z');
 
+frames = [1 1000 2000 3000 4000 5000 6000 7000 8000 9000];
 % Compute Moment of Inertia matrix
-I = pt2D_moi(w_vec2, w_dot_vec);
-
+I = pt2D_moi(w_vec2(:,frames), w_dot_vec(:,frames));
 
 % [ q,w,a ] = pt2E_traj(th2(:, end), w_vec2(:, end), w_dot_vec(:, end),...
 %                       I, tstep, 1000);
--- matlab_code/pt2D_moi.m
+++ matlab_code/pt2D_moi.m
@@ -4,21 +4,46 @@ function [ inertiaMtx ] = pt2D_moi( omega, alpha )
 % Output:   inertialMtx, moment of inertia matrix
 
 [~, n] = size(omega);
-optimA = [];
+A = zeros(3*n, 6);
 
-I = sym('I', [3 3]);
+% form matrix Ap = 0 to find moment of inertial matrix entries
+for i = 1:n
+    index = (i-1)*3;
+    mtx1 = ...
+        [alpha(1,i) 0 0 0 alpha(3,i) alpha(2,i);
+        0 alpha(2,i) 0 alpha(3,i) 0 alpha(1,i);
+        0 0 alpha(3,i) alpha(2,i) alpha(1,i) 0];
+    mtx2 = [0 -omega(3,i) omega(2,i); omega(3,i) 0 -omega(1,i); ...
+        -omega(2,i) omega(1,i) 0]* ...
+        [omega(1,i) 0 0 0 omega(3,i) omega(2,i); ...
+        0 omega(2,i) 0 omega(3,i) 0 omega(1,i);
+        0 0 omega(3,i) omega(2,i) omega(1,i) 0];
+    A(index+1:index+3,:) = mtx1 + mtx2;
+end
+
+[~,~,v] = svd(A);
+p = v(:,end);
+inertiaMtx = [p(1) p(6) p(5);
+              p(6) p(2) p(4);
+              p(5) p(4) p(3)];
+inertiaMtx(abs(inertiaMtx)<0.01) = 0;
+inertiaMtx = abs(inertiaMtx)
+% optimA = [];
+
+% I = sym('I', [3 3]);
 % alpha = sym('alpha', [3 1]);
 % omega = sym('omega', [3 1]);
 
-for i = 1:n
-    tau = I*alpha(:, i) + cross(omega(:, i),I*omega(:, i));
-    [A, ~] = equationsToMatrix([tau(1), tau(2), tau(3)], ...
-        [I(1,1), I(1,2), I(1,3), I(2,1), I(2,2), ...
-        I(2,3), I(3,1), I(3,2), I(3,3)]);
-    optimA = vertcat(optimA,A);
-end
-A = double(optimA)
-inertiaMtx = null(A)
-inertiaMtx = reshape(inertiaMtx(:, end),3,3)';
+% for i = 1:n
+%     tau = I*alpha(:, i) + cross(omega(:, i),I*omega(:, i));
+%     [A, ~] = equationsToMatrix([tau(1), tau(2), tau(3)], ...
+%         [I(1,1), I(1,2), I(1,3), I(2,1), I(2,2), ...
+%         I(2,3), I(3,1), I(3,2), I(3,3)]);
+%     optimA = vertcat(optimA,A);
+% end
+% A = double(optimA)
+% inertiaMtx = null(A)
+% inertiaMtx = reshape(inertiaMtx(:, end),3,3)';
+
 % what coordinate frame is this??
 end
