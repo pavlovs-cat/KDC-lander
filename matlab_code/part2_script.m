8a98e3e7a392f4708211fb4a98bba9572041feec
Cormac O'Meadhra <cormac.omeadhra@gmail.com>
2017-04-11 19:12:13 -0400

    Adding updated txt file

--- matlab_code/part2_script.m
+++ matlab_code/part2_script.m
@@ -45,55 +45,54 @@ for k = 2:length(all_time)
 end
 
 % Store quaternions for each timestamp
-artifact_rot = zeros(Nsim, 4);
 Rots = cat(3, eye(3), zeros(3, 3, Nsim));
-th2 = horzcat([1 0 0 0]',zeros(4, Nsim));
+th = horzcat([1 0 0 0]',zeros(4, Nsim));
 
 for j = 1:Nsim
     p = reshape(pos_vec(j, :), 3, 8);
     q = reshape(pos_vec(j+1, :), 3, 8);
     rot = pt2A_helper(p,q);
-    th2(:, j+1) = quatmultiply(rot, th2(:, j)')';
+    th(:, j+1) = quatmultiply(rot', th(:, j)')';
 end
 
 % Do janky swap
-th2 = th2(:, 2:end);
-th2 = [th2(1, :); -th2(3, :); th2(2, :); th2(4, :)];
+th = th(:, 2:end);
+th = [th(1, :); -th(3, :); th(2, :); th(4, :)];
 for j = 1:Nsim
-   Rots(:, :, j+1) = quat2rotm(th2(:, j)'); 
+   Rots(:, :, j+1) = quat2rotm(th(:, j)'); 
 end
 % Rots = Rots(:, :, 2:end);
 th_sim =  extract_data(data_file, 'a_q', {'0', '1', '2', '3'}, 1 : Nsim+N)';
 
 % Plot  for orientation
 if gen_plot == 1
-  plot_results(th_sim(1, 1:Nsim), th2(1, :), init_times, 'q_0');
-  plot_results(th_sim(2, 1:Nsim), th2(2, :), init_times, 'q_1');
-  plot_results(th_sim(3, 1:Nsim), th2(3, :), init_times, 'q_2');
-  plot_results(th_sim(4, 1:Nsim), th2(4, :), init_times, 'q_3');
+  plot_results(th_sim(1, 1:Nsim), th(1, :), init_times, 'q_0');
+  plot_results(th_sim(2, 1:Nsim), th(2, :), init_times, 'q_1');
+  plot_results(th_sim(3, 1:Nsim), th(3, :), init_times, 'q_2');
+  plot_results(th_sim(4, 1:Nsim), th(4, :), init_times, 'q_3');
 end
 
 % Extract simulated ang accels and velocities
 w_sim = extract_data(data_file, 'a_w', {'x', 'y', 'z'}, 1 : Nsim + N)';
 
 % Compute angular velocities
-w_vec2 = pt2B_angv2(Rots, tau);
+w_vec = pt2B_angv2(Rots, tau);
 
 % Plot velocities for comparison
 if gen_plot == 1
-  plot_results(w_sim(1, 1:Nsim), w_vec2(1, :), init_times, 'w_x');
-  plot_results(w_sim(2, 1:Nsim), w_vec2(2, :), init_times, 'w_y');
-  plot_results(w_sim(3, 1:Nsim), w_vec2(3, :), init_times, 'w_z');
+  plot_results(w_sim(1, 1:Nsim), w_vec(1, :), init_times, 'w_x');
+  plot_results(w_sim(2, 1:Nsim), w_vec(2, :), init_times, 'w_y');
+  plot_results(w_sim(3, 1:Nsim), w_vec(3, :), init_times, 'w_z');
 end
 
 % Compute angular accelerations
-w_dot_vec = pt2C_angacc(w_vec2, tau);
+w_dot_vec = pt2C_angacc(w_vec, tau);
 
 % Compute Moment of Inertia matrixs
-I = pt2D_moi(w_vec2, w_dot_vec)
+I = pt2D_moi(w_vec, w_dot_vec);
 
 % I = [0.1739 0 0; 0 0.5931  0; 0 0 0.7861]; % In WORLD frame
-[ q,w,a ] = pt2E_traj(th2(:, end-1), w_vec2(:, end-1), w_dot_vec(:, end-2),...
+[ q,w,a ] = pt2E_traj(th(:, end-1), w_vec(:, end-1), w_dot_vec(:, end-2),...
                       I, tau, N);
 if gen_plot == 1
     plot_results(w_sim(1, Nsim+1:Nsim+N), w(1, :), predict_times, 'w^p_x');
@@ -102,5 +101,12 @@ if gen_plot == 1
 end
 
 % Uncomment to write txt file again
-com_traj = horzcat(com_pos,vertcat([1 0 0 0], th2'));
-% dlmwrite('com_traj_lander.txt',com_traj,'delimiter',' ','precision',6)
+% Rotate values to assignment frame
+th_assign = th;
+quat_r = rotm2quat(R);
+for idx = 1 : size(th, 2)
+   th_assign(:, idx) = quatmultiply( quat_r',  th(:, idx)' )'; 
+end
+
+com_traj = horzcat(com_pos,vertcat([1 0 0 0], th_assign'));
+dlmwrite('com_traj_lander.txt',com_traj,'delimiter',' ','precision',6)
